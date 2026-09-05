7f392a816313ecac60d44b70505dbcda2b50d46a
Dr. YFish <261328128+dryfish09@users.noreply.github.com>
2026-09-05 19:08:34 +0700

    Ok

--- app/src/main/cpp/Main.cpp
+++ app/src/main/cpp/Main.cpp
@@ -487,8 +487,8 @@ extern "C" JNIEXPORT void JNICALL Java_com_android_support_Preferences_Changes(J
 extern "C" JNIEXPORT jobjectArray JNICALL Java_com_android_support_CkHomuraMenu_GetFeatureList(JNIEnv *env, jobject thiz) {
     const std::string language = homura::GetLocaleLanguage(env);
     const auto &featureList =                            //
-        (language == "zh") ? cheat::zh_Hans::featureList //
-        : (language == "vi") ? cheat::vi_VN::featureList //
+        (language == "vi") ? cheat::vi_VN::featureList   //
+        : (language == "zh") ? cheat::zh_Hans::featureList //
                              : cheat::en_US::featureList;
 
     const jsize featuresCount = static_cast<jsize>(featureList.size());
@@ -502,8 +502,8 @@ extern "C" JNIEXPORT jobjectArray JNICALL Java_com_android_support_CkHomuraMenu_
 extern "C" JNIEXPORT jobjectArray JNICALL Java_com_android_support_CkHomuraMenu_SettingsList(JNIEnv *env, jobject thiz) {
     const std::string language = homura::GetLocaleLanguage(env);
     const auto &settingsList =                            //
-        (language == "zh") ? cheat::zh_Hans::settingsList //
-        : (language == "vi") ? cheat::vi_VN::settingsList //
+        (language == "vi") ? cheat::vi_VN::settingsList   //
+        : (language == "zh") ? cheat::zh_Hans::settingsList //
                              : cheat::en_US::settingsList;
 
     const jsize settingsCount = static_cast<jsize>(settingsList.size());
